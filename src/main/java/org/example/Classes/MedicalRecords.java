aefa6913e8a5827653693e23c9e193ae64f62d9b
nishimwecelie <nishimwecelie@gmail.com>
2026-03-12 18:35:57 +0200

    medical class added

---
+++ src/main/java/org/example/Classes/MedicalRecords.java
@@ -0,0 +1,47 @@
+package org.example.Classes;
+
+public class MedicalRecords {
+    private int patiendId;
+    private String diagnosis ;
+    private String details;
+    private int doctorId;
+
+    public MedicalRecords(int patiendId, String diagnosis, String details, int doctorId) {
+        this.patiendId = patiendId;
+        this.diagnosis = diagnosis;
+        this.details = details;
+        this.doctorId = doctorId;
+    }
+
+    public int getPatiendId() {
+        return patiendId;
+    }
+
+    public void setPatiendId(int patiendId) {
+        this.patiendId = patiendId;
+    }
+
+    public String getDiagnosis() {
+        return diagnosis;
+    }
+
+    public void setDiagnosis(String diagnosis) {
+        this.diagnosis = diagnosis;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public int getDoctorId() {
+        return doctorId;
+    }
+
+    public void setDoctorId(int doctorId) {
+        this.doctorId = doctorId;
+    }
+}
