d197143572d993a63000e3e4138e149e52e43573
nishimwecelie <nishimwecelie@gmail.com>
2026-03-12 18:07:27 +0200

    initial_commit

---
+++ src/main/java/org/example/Classes/Doctors.java
@@ -0,0 +1,58 @@
+package org.example.Classes;
+
+public class Doctors {
+    private String firstName;
+    private String lastName;
+    private String speciality;
+    private String phoneNumber;
+    private String email;
+
+
+    public Doctors(String firstName, String lastName, String speciality, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.speciality = speciality;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getSpeciality() {
+        return speciality;
+    }
+
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/main/java/org/example/Classes/Patients.java
@@ -0,0 +1,67 @@
+package org.example.Classes;
+
+public class Patients {
+    private String firstName;
+    private String lastName;
+    private String dob;
+    private String gender;
+    private String phoneNumber;
+    private String email;
+
+    public Patients(String firstName, String lastName, String dob, String gender, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dob = dob;
+        this.gender = gender;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDob() {
+        return dob;
+    }
+
+    public void setDob(String dob) {
+        this.dob = dob;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
